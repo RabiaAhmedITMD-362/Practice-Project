fa773858235658216019805141ebb8d30bba327a
rsahmed16 <112486837+rsahmed16@users.noreply.github.com>
2023-03-04 21:41:14 -0600

    Update site.js

--- site.js
+++ site.js
@@ -1,33 +1,5 @@
-const loginForm = document.getElementById("login-form");
-const registerForm = document.getElementById("register-form");
-const loginBtn = document.getElementsByTagName("span")[0];
-const registerBtn = document.getElementsByTagName("span")[1];
-const indicator = document.getElementById("indicator");
-
-registerBtn.addEventListener("click", () => {
-  registerForm.style.display = "block";
-  loginForm.style.display = "none";
-  registerBtn.classList.add("active");
-  loginBtn.classList.remove("active");
-  indicator.style.width = registerBtn.offsetWidth + "px";
-  indicator.style.left = registerBtn.offsetLeft + "px";
-});
-
-loginBtn.addEventListener("click", () => {
-  registerForm.style.display = "none";
-  loginForm.style.display = "block";
-  loginBtn.classList.add("active");
-  registerBtn.classList.remove("active");
-  indicator.style.width = loginBtn.offsetWidth + "px";
-  indicator.style.left = loginBtn.offsetLeft + "px";
-});
-
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // Login form submit logic here
-});
-
-registerForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // Register form submit logic here
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+	$('html').removeClass('nojs');
+	$('html').addClass('hasjs');
 });
